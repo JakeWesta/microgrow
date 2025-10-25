368e66b373aa93501cbee24e3f89058e306ec771
JakeWesta <west.jake@ufl.edu>
2025-10-25 17:21:18 -0400

    Fixed merge for main, going to switch to new branch to develop

--- app/lib/mqtt_connect.dart
+++ app/lib/mqtt_connect.dart
@@ -1,2 +1,10 @@
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+
+
+// TODO: Implement class to support MQTT connection and 
+// subcribing to topics 
+
+class MqttConnect {
+
+}
