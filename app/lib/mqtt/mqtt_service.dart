fd6d4b995e8b872ff02ffb6d02b201bc826e3d72
JakeWesta <west.jake@ufl.edu>
2025-10-26 15:38:30 -0400

    revamped MQTT to be its own class. Made connect() its own function. added and tested a setupHabitat to init an ESP32. added sensorSubscribe to tap into ESP32 sensor topic.

--- app/lib/mqtt/mqtt_service.dart
+++ app/lib/mqtt/mqtt_service.dart
@@ -1,42 +1,68 @@
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_browser_client.dart';
+import 'dart:convert';
+import 'package:typed_data/typed_data.dart';
 
-Future<MqttClient> connect() async {
-  const host = 'broker.emqx.io';
-  const port = 8083;
-  const path = '/mqtt';
-  final url = 'ws://$host:$port$path';
 
-  final client = MqttBrowserClient(
-    url,
-    'flutter_web_${DateTime.now().millisecondsSinceEpoch}',
-  );
-  client.port = port;
-  client.websocketProtocols =
-      MqttClientConstants.protocolsSingleDefault; // ['mqtt']
-  client.keepAlivePeriod = 30;
-  client.setProtocolV311();
-  client.logging(on: true);
+class MqttService {
+  static const host = 'broker.emqx.io';
+  static const port = 8083;
+  static const path = '/mqtt';
+  static MqttBrowserClient? client;
+  
+  
+  static Future<MqttBrowserClient> connect() async {
+    final url = 'ws://$host:$port$path';
 
-  client.onConnected = () => print('Connected');
-  client.onDisconnected = () => print('Disconnected');
-  client.onSubscribed = (t) => print('Subscribed: $t');
+      final client = MqttBrowserClient(
+        url,
+        'flutter_web_${DateTime.now().millisecondsSinceEpoch}',
+      );
+      client.port = port;
+      client.websocketProtocols =
+          MqttClientConstants.protocolsSingleDefault; // ['mqtt']
+      client.keepAlivePeriod = 30;
+      client.setProtocolV311();
+      client.logging(on: true);
 
-  final conn = MqttConnectMessage()
-      .startClean()
-      .withWillTopic('app/status')
-      .withWillMessage('offline')
-      .withWillQos(MqttQos.atLeastOnce);
-  client.connectionMessage = conn;
+      client.onConnected = () => print('Connected');
+      client.onDisconnected = () => print('Disconnected');
+      client.onSubscribed = (t) => print('Subscribed: $t');
 
-  print('Connecting to $url ...');
-  await client.connect();
+      final conn = MqttConnectMessage()
+          .startClean()
+          .withWillTopic('app/status')
+          .withWillMessage('offline')
+          .withWillQos(MqttQos.atLeastOnce);
+      client.connectionMessage = conn;
 
-  if (client.connectionStatus?.state != MqttConnectionState.connected) {
-    final rc = client.connectionStatus?.returnCode;
-    client.disconnect();
-    throw Exception('MQTT not connected. CONNACK=$rc');
+      print('Connecting to $url ...');
+      await client.connect();
+
+      if (client.connectionStatus?.state != MqttConnectionState.connected) {
+        final rc = client.connectionStatus?.returnCode;
+        client.disconnect();
+        throw Exception('MQTT not connected. CONNACK=$rc');
+      }
+
+      client.subscribe('microgrow/init', MqttQos.atLeastOnce);
+
+      return client;
+
   }
 
-  return client;
+  static Future<void> setupHabitat({required String habitatId, required String greenType, required int schedule}) async {
+    final client = await connect();
+    final topic = 'microgrow/init';
+
+    final msg = jsonEncode({
+      'id': habitatId,
+      'green': greenType,
+      'schedule': schedule
+    });
+    client.publishMessage(topic, MqttQos.atLeastOnce, Uint8Buffer()..addAll(utf8.encode(msg)));
+  }
+
+  
 }
+
