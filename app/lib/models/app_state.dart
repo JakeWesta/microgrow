721df2f099e6e38b90471a771c71ea9f454b9104
Jake West <west.jake@ufl.edu>
2025-11-13 16:44:42 -0500

    made it possbile to delete habitats on app state side

--- app/lib/models/app_state.dart
+++ app/lib/models/app_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'habitat_obj.dart';
 import 'database.dart';
 
@@ -11,9 +12,16 @@ class MyAppState extends ChangeNotifier {
 
   List<Habitat> get getHabitats => habitats;
 
-  void addHabitat(Habitat habitat) {
-    Database.saveHabitat(habitat);
+  void addHabitat(Habitat habitat) async {
+    await Database.saveHabitat(habitat);
     habitats = Database.box.values.toList();
     notifyListeners();
   }
+
+    void deleteHabitat(Habitat habitat) async {
+    await Database.box.delete(habitat.id);
+    habitats = Database.box.values.toList();
+    notifyListeners();
+  }
+
 }
